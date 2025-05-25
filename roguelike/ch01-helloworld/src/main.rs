3d0fc7447aaccc1fbb3ee3460b4fbbcac94e87fd
Mr_Taki <2028566723@qq.com>
2025-05-25 21:44:23 +0800

    src: 在GUI中打印hello-world

---
+++ roguelike/ch01-helloworld/src/main.rs
@@ -0,0 +1,23 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+//为State实现GameState trait
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        for i in 1..=5 {
+            ctx.draw_box(25 + i, 15 + i, 30 - i * 2, 20 - i * 2, WHITE, BLACK);
+        }
+        ctx.print_color_centered(25, YELLOW, BLACK, "Hello world!");
+    }
+}
+
+fn main() -> BError {
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Roguelike game")
+        .build()?;
+
+    let gs = State {};
+    main_loop(ctx, gs)
+}
